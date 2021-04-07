ae193edb1e71a4b02fa6cdb02089d6dae4c74e85
datatags <datatags@users.noreply.github.com>
2021-04-07 16:03:43 -0700

    Clean some things up

--- src/main/java/over2craft/betterlectern/BookListener.java
+++ src/main/java/over2craft/betterlectern/BookListener.java
@@ -2,6 +2,7 @@ package over2craft.betterlectern;
 
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Lectern;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
@@ -9,62 +10,31 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 public class BookListener implements Listener {
-
+    private static final BlockFace[] SIDES = {BlockFace.EAST, BlockFace.NORTH, BlockFace.SOUTH, BlockFace.WEST};
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
-        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-            if (e.getClickedBlock() != null && e.getClickedBlock().getState() instanceof Lectern) {
-
-                if (hasSignNear(e.getClickedBlock())) {
-                    Lectern lectern = (Lectern) e.getClickedBlock().getState();
-
-                    if (lectern.getInventory().getItem(0) != null) {
-                        e.getPlayer().openBook(Objects.requireNonNull(lectern.getInventory().getItem(0)));
-                        e.setCancelled(true);
-                    }
-                }
-            }
-        }
+        if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (!(e.getClickedBlock().getState() instanceof Lectern)) return;
+        if (!hasSignNear(e.getClickedBlock())) return;
+        Lectern lectern = (Lectern) e.getClickedBlock().getState();
+        if (lectern.getInventory().getItem(0) == null) return;
+        
+    	e.setCancelled(true);
+        e.getPlayer().openBook(Objects.requireNonNull(lectern.getInventory().getItem(0)));
     }
 
     public boolean hasSignNear(Block block) {
-        if (block.getRelative(BlockFace.EAST).getState() instanceof Sign) {
-            Sign sign = (Sign) block.getRelative(BlockFace.EAST).getState();
-            if (containsKey(sign)) {
-                return true;
-            }
-        }
-
-        if (block.getRelative(BlockFace.NORTH).getState() instanceof Sign) {
-            Sign sign = (Sign) block.getRelative(BlockFace.NORTH).getState();
-            if (containsKey(sign)) {
-                return true;
-            }
-        }
-
-        if (block.getRelative(BlockFace.SOUTH).getState() instanceof Sign) {
-            Sign sign = (Sign) block.getRelative(BlockFace.SOUTH).getState();
-            if (containsKey(sign)) {
-                return true;
-            }
-        }
-
-        if (block.getRelative(BlockFace.WEST).getState() instanceof Sign) {
-            Sign sign = (Sign) block.getRelative(BlockFace.WEST).getState();
-            return containsKey(sign);
-        }
-
-        return false;
-    }
-
-    public boolean containsKey(Sign sign) {
-        for (String line : sign.getLines()) {
-            if (line.contains("betterLectern")) {
-                return true;
+        for (BlockFace side : SIDES) {
+            BlockState state = block.getRelative(side).getState();
+            if (!(state instanceof Sign)) continue;
+            Sign sign = (Sign) state;
+            for (String line : sign.getLines()) {
+                if (line.contains("betterLectern")) {
+                    return true;
+                }
             }
         }
         return false;
